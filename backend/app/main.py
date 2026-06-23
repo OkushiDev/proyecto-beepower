e3e505ebb3c53104a728df57105e640c58f47dc0
OkushiTamaru <ricardo.benavides1401@gmail.com>
2026-06-23 16:17:34 -0400

    feat(main): añade api a main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.api import users  # Importamos el módulo de rutas
+from app.api import users, auth 
+
 
 app = FastAPI(title="BeePower API", version="0.1.0")
 
-# Conectamos el enrutador de usuarios a la red. 
-# Todas las rutas dentro de ese archivo tendrán el prefijo "/usuarios"
+app.include_router(auth.router, prefix="/auth", tags=["Autenticación"])
 app.include_router(users.router, prefix="/usuarios", tags=["Usuarios"])
 
 
