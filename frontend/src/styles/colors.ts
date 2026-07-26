2ceb7ad17b573cb14ecfca9b38adf74379a8729c
OkushiDev <ricardo.benavides1401@gmail.com>
2026-07-26 19:35:09 -0400

    Crea colors.ts con colores para estilos

---
+++ frontend/src/styles/colors.ts
@@ -0,0 +1,78 @@
+const COLOR_PRIMARY = '#1976d2'; // Blue
+const COLOR_PRIMARY_LIGHT = '#42a5f5';
+const COLOR_PRIMARY_DARK = '#1565c0';
+const COLOR_SECONDARY = '#9c27b0'; // Purple
+const COLOR_SUCCESS = '#388e3c'; // Green
+const COLOR_WARNING = '#f57c00'; // Orange
+const COLOR_ERROR = '#d32f2f'; // Red
+const COLOR_INFO = '#0288d1'; // Light Blue
+const COLOR_LIGHT = '#f5f5f5'; // Light Grey
+const COLOR_BORDER = '#e0e0e0'; // Light Border Grey
+const COLOR_DARK = '#212121'; // Dark Grey
+const COLOR_TEXT_PRIMARY = '#212121'; // Almost Black
+const COLOR_TEXT_SECONDARY = '#757575'; // Medium Grey
+const COLOR_TEXT_DISABLED = '#bdbdbd'; // Light Grey
+const COLOR_BACKGROUND_DEFAULT = '#fff'; // Very Light Grey
+const COLOR_BACKGROUND_PAPER = '#ffffff'; // White
+const COLOR_BACKGROUND_INOCULUMS = '#ede7f6'; // Light Blue for Inoculums Accordion
+const COLOR_BACKGROUND_TREATMENTS = '#fce4ec'; // Light Pink for Treatments Accordion
+const COLOR_BACKGROUND_SCHEDULES = '#e8f5e9'; // Light Green for Schedules Accordion
+const COLOR_BACKGROUND_ACTIVES = '#fff3e0'; // Light Orange for Actives Accordion
+const COLOR_BACKGROUND_GROUPS = '#e3f2fd'; // Light Purple for Groups Accordion
+const COLOR_BACKGROUND_VARIABLES = '#f3e5f5'; // Light Lavender for Variables Accordion
+
+const COLOR_WHATSAPP = '#25D366';
+
+const COLOR_APP_BAR = '#3DAE2C';
+const COLOR_DASHBOARD_MOBILE_DRAWER = '#000000';
+
+const COLOR_SIDEBAR_BACKGROUND = '#357e04';
+
+const COLOR_LOGIN_PRIMARY = '#4caf50';
+const COLOR_LOGIN_PRIMARY_LIGHT = '#80e27e';
+const COLOR_LOGIN_PRIMARY_DARK = '#087f23';
+const COLOR_LOGIN_BACKGROUND = '#e8f5e9';
+
+const COLOR_TABLE_BORDER = '#cccccc';
+const COLOR_TEXT_BODY = 'rgba(0, 0, 0, 0.87)';
+const COLOR_MEASUREMENT_CELL_VALID = 'hsla(137, 74%, 31%, 0.2)';
+const COLOR_MEASUREMENT_CELL_INVALID = 'hsla(0, 74%, 31%, 0.2)';
+const COLOR_MEASUREMENT_CELL_SELECTED = 'hsla(217, 74%, 50%, 0.25)';
+
+export const colors = {
+  whatsapp: COLOR_WHATSAPP,
+  appBar: COLOR_APP_BAR,
+  dashboardMobileDrawer: COLOR_DASHBOARD_MOBILE_DRAWER,
+  sidebarBackground: COLOR_SIDEBAR_BACKGROUND,
+  loginPrimary: COLOR_LOGIN_PRIMARY,
+  loginPrimaryLight: COLOR_LOGIN_PRIMARY_LIGHT,
+  loginPrimaryDark: COLOR_LOGIN_PRIMARY_DARK,
+  loginBackground: COLOR_LOGIN_BACKGROUND,
+  primary: COLOR_PRIMARY,
+  primaryLight: COLOR_PRIMARY_LIGHT,
+  primaryDark: COLOR_PRIMARY_DARK,
+  secondary: COLOR_SECONDARY,
+  success: COLOR_SUCCESS,
+  warning: COLOR_WARNING,
+  error: COLOR_ERROR,
+  info: COLOR_INFO,
+  light: COLOR_LIGHT,
+  border: COLOR_BORDER,
+  dark: COLOR_DARK,
+  textPrimary: COLOR_TEXT_PRIMARY,
+  textSecondary: COLOR_TEXT_SECONDARY,
+  textDisabled: COLOR_TEXT_DISABLED,
+  backgroundDefault: COLOR_BACKGROUND_DEFAULT,
+  backgroundPaper: COLOR_BACKGROUND_PAPER,
+  backgroundInoculums: COLOR_BACKGROUND_INOCULUMS,
+  backgroundTreatments: COLOR_BACKGROUND_TREATMENTS,
+  backgroundSchedules: COLOR_BACKGROUND_SCHEDULES,
+  backgroundActives: COLOR_BACKGROUND_ACTIVES,
+  backgroundGroups: COLOR_BACKGROUND_GROUPS,
+  backgroundVariables: COLOR_BACKGROUND_VARIABLES,
+  tableBorder: COLOR_TABLE_BORDER,
+  textBody: COLOR_TEXT_BODY,
+  measurementCellValid: COLOR_MEASUREMENT_CELL_VALID,
+  measurementCellInvalid: COLOR_MEASUREMENT_CELL_INVALID,
+  measurementCellSelected: COLOR_MEASUREMENT_CELL_SELECTED,
+};
